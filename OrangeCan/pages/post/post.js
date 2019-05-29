05038265b9987914aa00935646d54d9950722432
JopLin5 <jopfree@163.com>
2019-05-29 12:18:11 +0800

    硬编码阶段

---
+++ OrangeCan/pages/post/post.js
@@ -0,0 +1,73 @@
+// pages/post/post.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    date:"Jun 28 2017",
+    title:"小时候的冰棍和雪糕",
+    postImg:"../../images/avatar/avatar-4.png",
+    avatar:"../../images/post/post-5.jpg",
+    content:"冰棍与雪糕，绝对不是同一个东西。3到5毛钱的雪糕犹如现在的哈根达斯，而5分1毛的冰棍儿就像现在的老冰棍。时过境移，...",
+    readingNum:92,
+    collectionNum:108,
+    comment:7,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
